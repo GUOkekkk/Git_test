d1bf7e38339371e5ac5ce0c2baeadb89093ad9a6
Your Name <you@example.com>
2021-03-18 15:57:52 +0800

    clone the exercise

---
+++ fm_branching_class_repo/main.py
@@ -0,0 +1,22 @@
+# Names of tea company employees:
+names = ['Leaf', 'Mailk', 'Cake', 'Jammy', 'Spoonah']
+# Surnames of tea company employees:
+surnames = ['Sugaro', 'Cuppo', 'Creamoff', 'Bru', 'Lipton']
+
+
+# ========= Taks 2: Make a hotfix ===========
+# Find the error in the following code and fix within branch 'hotfix'.
+
+# Print out the names and surnames of all the five employees
+# in format "Name Surname". At the moment, for some reason, they are
+# printed as "NameSurname" without a space, which is wrong.
+for i in range(len(names)):
+    print(names[i] + surnames[i])
+
+
+
+# ========= Taks 3: Implement a feature ===========
+# Print all 25 combinations of employees' names and surnames.
+# Commit this within branch 'feature' and then merge to 'master'
+
+# Your code goes here:
